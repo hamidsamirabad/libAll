a9e951e64f7807559c3a45e2d53478122a1daeef
Hamid <mr.hamidsamir@gmail.com>
2020-01-18 17:48:05 +0330

    NightMode

--- samir/src/main/java/ir/git/samir/Lib.java
+++ samir/src/main/java/ir/git/samir/Lib.java
@@ -3,11 +3,29 @@ package ir.git.samir;
 import android.content.Context;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import ir.git.samir.Utils.ClassPreferences;
 
 public class Lib {
 
     public static AppCompatActivity activity;
     public static Context context;
     public static View view;
+
+
+    public static boolean isNightMode = false;
+
+
+
+    public void init(){
+        ClassPreferences pref= new ClassPreferences(context);
+        isNightMode = pref.getBoolean("nightMode");
+
+    }
+
+
+    public void setIsNightMode(boolean isNightMode){
+        ClassPreferences pref= new ClassPreferences(context);
+        pref.set("nightMode",isNightMode);
+    }
 
 }
