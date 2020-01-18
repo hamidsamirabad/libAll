3d630f7d824813a82714d61ec86b108246228a14
Hamid <mr.hamidsamir@gmail.com>
2020-01-18 17:28:38 +0330

    fourth

---
+++ samir/src/main/java/ir/git/samir/Enums/FontTypes.kt
@@ -0,0 +1,9 @@
+package ir.git.samir.Enums
+
+enum class FontTypes(var value: String) {
+
+    IRANSANS_NORMAL("fonts/fa.ttf"),
+    IRANSANS_Medium("fonts/IRANSansMobile_Medium.ttf"),
+    IRANSANS_Bold("fonts/IRANSansMobile_Bold.ttf")
+
+}
